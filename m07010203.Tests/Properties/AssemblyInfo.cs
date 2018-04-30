7123c234d72a236a1ca9b341ff40af6b4ef01034
lizasixsixsix <lizasixsixsix@gmail.com>
2018-04-30 21:19:47 +0300

    added to remote

---
+++ m07010203.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("m07010203.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MICROSOFT")]
+[assembly: AssemblyProduct("m07010203.Tests")]
+[assembly: AssemblyCopyright("Copyright © MICROSOFT 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("00cc0dab-2acb-4627-913d-0c372953d0cf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
